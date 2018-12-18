f02484807b2d37b980794272446299b3b1854827
Brian Murillo <brian.j.murillo@gmail.com>
2018-12-18 19:42:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,14 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   # return planeteer_calls
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(short_words)# code an argument here
   # Your code here
+  short_words.each do |word|
+    if word.length >4
+      return true
+    end
+  end
+  return false
 end
 
 def find_the_cheese# code an argument here
